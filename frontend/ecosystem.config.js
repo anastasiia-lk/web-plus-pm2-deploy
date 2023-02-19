e12398c8801d16be48400fc357a663437e0fbe40
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2023-02-19 18:31:03 +0300

    add frontend config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'app-frontend',
+      script: './build/index.html',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy':
+        'cd ~/web-plus-pm2-deploy/frontend/source/frontend && npm i && npm run build'
+    },
+  },
+};
